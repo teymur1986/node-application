4ab529b7ac7ac3e34e68ee05fd3c4a5689e3e128
Timot <timortartakovsky@gmail.com>
2019-11-01 22:22:59 +0200

    init files

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+// routes import
+const adminRoute = require('./routes/admin');
+const shoppingRoute = require('./routes/shop');
+// routes import
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use('/admin', adminRoute);
+app.use(shoppingRoute);
+// routes
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
+
---
+++ src/routes/admin.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
